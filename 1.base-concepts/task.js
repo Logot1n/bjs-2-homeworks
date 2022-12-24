b4ee9e96af4d60aacb8fd6ac7d8d595233f04c29
Logot1n <miwutka2@gmail.com>
2022-12-24 13:01:14 +0300

    first theme is Ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,26 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = ["х1", "х2"];
+  let arr = [];
   let d = b**2-4*a*c;
   if (d > 0) {
     arr[0] = (-b + Math.sqrt(d))/(2*a);
     arr[1] = (-b - Math.sqrt(d))/(2*a);
-    return arr;
   } else if (d == 0) {
-    arr = ["x1"];
     arr[0] = -b/(2*a);
-    return arr;
-  } else if (d < 0) {
-    return arr = [];
   }
+  return arr;
 }
 solveEquation(1, 5, 4)
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false;
+  }
   let P = percent/100/12;
   let S = amount - contribution;
   let amountPerMonth = S * (P + (P / (((1 + P)**countMonths) - 1)));
-  let totalAmount = amountPerMonth * countMonths;
-  return totalAmount.toFixed(2);
+  let totalAmount =+ amountPerMonth * countMonths;
+  return Number(totalAmount.toFixed(2));
 }
 calculateTotalMortgage(10, 0, 10000, 36)
