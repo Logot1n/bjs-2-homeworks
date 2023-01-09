dc4441186c640b9d2f529eddee9353008895e46b
Logot1n <miwutka2@gmail.com>
2023-01-09 15:49:15 +0300

    3 тема сделана на 50%

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    if(compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i])) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = people.filter(user => user.gender === "мужской").map(user => user.age).reduce((acc, item, index) => {
+    acc += item;
+    if(index === people.length - 1) {
+        return acc/people.length;
+    }
+    return acc;
+  }, 0)
 }
