f584f85434849992706ff5a68e86604c769ee86a
Logot1n <miwutka2@gmail.com>
2023-01-11 16:40:20 +0300

    Не понимаю 2 задание в 3 теме

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,7 @@
 function compareArrays(arr1, arr2) {
-    if(compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i])) {
-        return true;
-    } else {
-        return false;
-    }
+    return arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  let result = people.filter(user => user.gender === "мужской").map(user => user.age).reduce((acc, item, index) => {
-    acc += item;
-    if(index === people.length - 1) {
-        return acc/people.length;
-    }
-    return acc;
-  }, 0)
+  let result = users.filter(user => user.gender === gender).map(user => user.age).reduce()
 }
