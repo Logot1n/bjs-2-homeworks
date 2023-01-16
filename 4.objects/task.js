be950c5d7f55d28897904c2f7c66f5207e139f98
Logot1n <miwutka2@gmail.com>
2023-01-16 23:33:54 +0300

    Ошибка в проверке оценок

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,13 +1,20 @@
 function Student(name, gender, age) {
-  
+    this.marks = [];
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this.hasOwnProperty("marks") === false) {
+        console.log("Добавление невозможно, пользователь отчислен");
+    } else {
+        this.marks = [...marks];
+    }
 }
 
 Student.prototype.getAverage = function () {
